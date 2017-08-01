9edd5470fb8b2c3c65d9fb8837b19b44ced79569
Chris <calandryll@gmail.com>
2017-08-01 14:26:36 -0400

    Update

---
+++ fastq_id_parser.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# From: https://www.biostars.org/p/10353/
+import sys
+
+#get fname from parameter
+idfile=sys.argv[1]
+
+#load ids
+ids = set( x.strip() for x in open(idfile) )
+
+#read stdid 
+handle=sys.stdin  
+
+while ids:
+  #parse fastq
+  idline=handle.readline()
+  seq   =handle.readline()
+  spacer=handle.readline()
+  quals =handle.readline()
+  #check
+  id=idline[:-1]
+  if id in ids:
+    #print fastq
+    sys.stdout.write( '%s%s%s%s' % ( idline, seq, spacer, quals ) )
+    #update ids
+    ids.remove( id )
