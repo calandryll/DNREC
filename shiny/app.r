7509ab6e0ca32e73557821b9c791c157a3b4d3e0
Chris <calandryll@gmail.com>
2017-08-07 07:20:35 -0400

    Updates

--- shiny/app.r
+++ shiny/app.r
@@ -39,24 +39,24 @@ server = function(input, output) {
     
       print(st.plot)
     })
-  output$hover_info <- renderUI({
-    hover <- input$plot_hover
-    point <- nearPoints(st2, hover, threshold = 5, maxpoints = 1, addDist = TRUE)
+  output$hover_info = renderUI({
+    hover = input$plot_hover
+    point = nearPoints(st2, hover, threshold = 5, maxpoints = 1, addDist = TRUE)
     if (nrow(point) == 0) return(NULL)
     
     # calculate point position INSIDE the image as percent of total dimensions
     # from left (horizontal) and from top (vertical)
-    left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
-    top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
+    left_pct = (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
+    top_pct = (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
     
     # calculate distance from left and bottom side of the picture in pixels
-    left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)
-    top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
+    left_px = hover$range$left + left_pct * (hover$range$right - hover$range$left)
+    top_px = hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
     
     # create style property fot tooltip
     # background color is set so tooltip is a bit transparent
     # z-index is set so we are sure are tooltip will be on top
-    style <- paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
+    style = paste0("position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
                     "left:", left_px + 2, "px; top:", top_px + 2, "px;")
     
     # actual tooltip created as wellPanel
